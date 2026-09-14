f4e1b1b957c74092b0146fa00940cf1beb54b25e
Joshua-shin123 <joshua.shin0301@gmail.com>
2026-09-14 21:58:50 +0000

    update

--- Java/000_Labs/006_doubles/base_code/starter.java
+++ Java/000_Labs/006_doubles/base_code/starter.java
@@ -9,7 +9,10 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		double qwertyuiop = (double)
-		System.out.print("I love to learn coding remotely."); 
+		double a = 5.0;
+		double b = 9.0;
+		double c = 32.0;
+		double qwertyuiop = c + a / b;
+		System.out.print(qwertyuiop); 
 	}
 }
--- Java/000_Labs/008_input/base_code/starter.java
+++ Java/000_Labs/008_input/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Joshua
+ *  Date: 9/10/26
 */
 
 import java.util.Scanner;
@@ -9,6 +9,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		Scanner sc = new Scanner (System.in);
+		System.out.println("What is your name");
+		String name = ();
+		System.out.println("What is your age?");
+
+
 	}
 }
